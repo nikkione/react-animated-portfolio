50980fee39b277f7f63b0f565184ac7bbb81b061
Nikita <xniki4x@gmail.com>
2024-02-01 22:48:17 +0300

    parallax added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import Test from "./Test";
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 const App = () => {
-  return (
-    <div>
-      <section id="Homepage">
-        <Navbar />
-        <Hero />
-      </section>
-      <section id="Homepage">Parallax</section>
-      <section id="Services">HERO</section>
-      <section id="Portfolio">HERO</section>
-      <section id="Contact">HERO</section>
-      <section id="About">HERO</section>
-      {/* <Test />
+	return (
+		<div>
+			<section id="Homepage">
+				<Navbar />
+				<Hero />
+			</section>
+			<section id="Services">
+				<Parallax type="services" />
+			</section>
+			<section>HERO</section>
+			<section id="Portfolio">
+				<Parallax type="portfolio" />
+			</section>
+			<section id="Contact">HERO</section>
+			<section id="About">HERO</section>
+			{/* <Test />
 			<Test /> */}
-    </div>
-  );
+		</div>
+	);
 };
 
 export default App;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,24 +1,83 @@
 import "./hero.scss";
+import { motion } from "framer-motion";
 
+const textVariants = {
+	initial: {
+		x: -500,
+		opacity: 0,
+	},
+	animate: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			duration: 1,
+			staggerChildren: 0.1,
+		},
+	},
+	scrollButton: {
+		opacity: 0,
+		y: 10,
+		transition: {
+			duration: 2,
+			repeat: Infinity,
+		},
+	},
+};
+const sliderVariants = {
+	initial: {
+		x: 0,
+	},
+	animate: {
+		x: "-220%",
+		opacity: 1,
+		transition: {
+			duration: 20,
+			repeat: Infinity,
+			repeatType: "mirror",
+		},
+	},
+};
 const Hero = () => {
-  return (
-    <div className="hero">
-      <div className="wrapper">
-        <div className="textContainer">
-          <h2>Nikita OPNMNDS</h2>
-          <h1>Web developer and UI designer</h1>
-          <div className="buttons">
-            <button>See The Latest Works</button>
-            <button>Contact Me</button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
-        <div className="imageContainer">
-          <img src="/hero.png" alt="" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="hero">
+			<div className="wrapper">
+				<motion.div
+					className="textContainer"
+					variants={textVariants}
+					initial="initial"
+					animate="animate"
+				>
+					<motion.h2 variants={textVariants}>Nikita OPNMNDS</motion.h2>
+					<motion.h1 variants={textVariants}>
+						Web developer and UI designer
+					</motion.h1>
+					<motion.div variants={textVariants} className="buttons">
+						<motion.button variants={textVariants}>
+							See The Latest Works
+						</motion.button>
+						<motion.button variants={textVariants}>Contact Me</motion.button>
+					</motion.div>
+					<motion.img
+						animate="scrollButton"
+						variants={textVariants}
+						src="/scroll.png"
+						alt=""
+					/>
+				</motion.div>
+				<motion.div
+					variants={sliderVariants}
+					initial="initial"
+					animate="animate"
+					className="slidingTextContainer"
+				>
+					Writer Content Creator Influencer
+				</motion.div>
+				<div className="imageContainer">
+					<img src="/hero.png" alt="" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
