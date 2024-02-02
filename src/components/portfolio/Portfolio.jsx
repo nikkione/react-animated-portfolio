6b8b5e3050d7e36a2db60b3a64c823ef784d4acd
Nikita Andreevich <xniki4x@gmail.com>
2024-02-02 17:48:53 +0300

    rest of app

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,82 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "React Commerce",
+    img: "https://images.pexels.com/photos/11107635/pexels-photo-11107635.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deleniti numquam tempore voluptates dignissimos fugiat adipisci maiores, similique expedita doloremque! Excepturi, nulla exercitationem in mollitia veritatis quod maiores optio nesciunt.",
+  },
+  {
+    id: 2,
+    title: "Next.js Blog",
+    img: "https://images.pexels.com/photos/19844371/pexels-photo-19844371.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deleniti numquam tempore voluptates dignissimos fugiat adipisci maiores, similique expedita doloremque! Excepturi, nulla exercitationem in mollitia veritatis quod maiores optio nesciunt.",
+  },
+  {
+    id: 3,
+    title: "Vanilla JS App",
+    img: "https://images.pexels.com/photos/20003297/pexels-photo-20003297.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deleniti numquam tempore voluptates dignissimos fugiat adipisci maiores, similique expedita doloremque! Excepturi, nulla exercitationem in mollitia veritatis quod maiores optio nesciunt.",
+  },
+  {
+    id: 4,
+    title: "Music App",
+    img: "https://images.pexels.com/photos/1084541/pexels-photo-1084541.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deleniti numquam tempore voluptates dignissimos fugiat adipisci maiores, similique expedita doloremque! Excepturi, nulla exercitationem in mollitia veritatis quod maiores optio nesciunt.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
